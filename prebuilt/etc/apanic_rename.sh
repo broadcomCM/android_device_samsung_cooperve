b764eed73581ba561e943a7b14d8d2e2862a9711
bieltv3 <bieltv.3@gmail.com>
2013-01-27 11:22:24 +0100

    big cleasnup on prebuilt folder -> vendor

--- prebuilt/etc/apanic_rename.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-
-/system/bin/mv /data/dontpanic/apanic_threads  /data/dontpanic/apanic_threads_`date +%d%m%y_%H%M%S`.txt
-/system/bin/mv /data/dontpanic/apanic_console  /data/dontpanic/apanic_console_`date +%d%m%y_%H%M%S`.txt
-/system/bin/mv /data/dontpanic/cp_dump  /data/dontpanic/cp_dump_`date +%d%m%y_%H%M%S`.bin
