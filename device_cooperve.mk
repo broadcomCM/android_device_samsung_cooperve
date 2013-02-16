82ace5d6666942e494018c929377879e1a31421b
bieltv3 <bieltv.3@gmail.com>
2013-02-16 12:16:21 +0100

    cleanup - thanks squadzone

--- device_cooperve.mk
+++ device_cooperve.mk
@@ -18,7 +18,7 @@
     $(call inherit-product, build/target/product/full_base.mk)
 
 # Add device package overlay
-    DEVICE_PACKAGE_OVERLAYS += device/samsung/cooperve/overlay
+    DEVICE_PACKAGE_OVERLAYS := device/samsung/cooperve/overlay
 
 # The gps config appropriate for this device
     $(call inherit-product, device/common/gps/gps_us_supl.mk)
@@ -371,3 +371,8 @@ PRODUCT_COPY_FILES += \
 # of the aspects that require proprietary drivers that aren't
 # commonly available
 $(call inherit-product-if-exists, vendor/samsung/cooperve/cooperve-vendor.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := cooperve
+PRODUCT_DEVICE := cooperve
+PRODUCT_MODEL := GT-S5830i
