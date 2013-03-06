1f82917be0a9cc10e509c90a1b6389237551a3bc
bieltv3 <bieltv.3@gmail.com>
2013-03-06 12:36:46 +0100

    make sources more compatible with other devices

--- device_cooperve.mk
+++ device_cooperve.mk
@@ -12,11 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-# Call common.mk
+# Inherit products
 $(call inherit-product, device/samsung/bcm21553-common/common.mk)
-
-# Inherit from those products. Most specific first.
+$(call inherit-product, vendor/samsung/cooperve/vendor_blobs.mk)
 $(call inherit-product, build/target/product/languages_full.mk)
 $(call inherit-product, build/target/product/full_base.mk)
 
@@ -48,13 +46,6 @@ PRODUCT_COPY_FILES += \
     device/samsung/cooperve/ramdisk/ueventd.gt-s5830i.rc:root/ueventd.gt-s5830i.rc \
     device/samsung/cooperve/ramdisk/init.gt-s5830i.rc:root/init.gt-s5830i.rc
 
-# Use the non-open-source parts, if they're present
--include vendor/samsung/cooperve/BoardConfigVendor.mk
-
-# Bootanimation
-PRODUCT_COPY_FILES += \
-    vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
-
 # Other kernel modules not in ramdisk
 ifeq ($(TARGET_PREBUILT_KERNEL),)
     LOCAL_KERNEL := device/samsung/cooperve/prebuilt/kernel
@@ -64,9 +55,3 @@ endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
-
-# See comment at the top of this file. This is where the other
-# half of the device-specific product definition file takes care
-# of the aspects that require proprietary drivers that aren't
-# commonly available
-$(call inherit-product-if-exists, vendor/samsung/cooperve/cooperve-vendor.mk)
