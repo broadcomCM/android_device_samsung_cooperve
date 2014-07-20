a2b35519242e979d4d3104b5c2505022b4d4ee1c
bieltv3 <bieltv.3@gmail.com>
2014-07-20 15:58:55 +0200

    Fix brunch and update

--- full_cooperve.mk
+++ full_cooperve.mk
@@ -41,7 +41,7 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_cooperve
+PRODUCT_NAME := full_cooperve
 PRODUCT_DEVICE := cooperve
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-S5830i
